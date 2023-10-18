f0f0e2e7d5242f5122befc47bb80dcfdd84f1730
B_Burgess <140908055+Billy-Burgess@users.noreply.github.com>
2023-10-18 14:17:55 +0100

    Learning & Organising
    Organising the folders by which ones I want to upload to GitHub.

---
+++ JavaScript/Coding Exercises 38-46.js
@@ -0,0 +1,75 @@
+// 38 Return Value Practice:
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// 39 isShortsWeather Function:
+function isShortsWeather(temperature) {
+    if (temperature >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 40 Last Element Exercise:
+function lastElement(arr) {
+    if (arr.length === 0) {
+        return null;
+    } else {
+        return arr[arr.length - 1];
+    }
+}
+
+// 41 Capitalize Exercise:
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+// 42 Sum Array Exercise:
+function sumArray(nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    return sum;
+}
+
+// 43 Days Of The Week Exercise:
+function returnDay(num) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    if (num < 1 || num > 7) {
+        return null;
+    } else {
+        return days[num - 1];
+    }
+}
+
+// 44 Function Expression Exercise:
+let square = function (num) {
+    return num * num;
+}
+
+// 45 Methods Exercise:
+let square = {
+    area(side) {
+        return side * side;
+    },
+    perimeter(side) {
+        return side * 4;
+    }
+}
+
+console.log(square.area(10));
+console.log(square.perimeter(10));
+
+// 46 Egg Laying Exercise:
+let hen = {
+    name: 'Helen',
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount++;
+        return 'EGG';
+    }
+};
+
